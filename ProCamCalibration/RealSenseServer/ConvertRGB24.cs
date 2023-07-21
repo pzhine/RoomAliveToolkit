522572d9534550d478e6435765e971d0da638bb7
Paul Hine <paul.hine@gmail.com>
2023-07-21 16:29:09 +0100

    Create RealSenseServer following the interface defined by KinectServer2 (WIP)

---
+++ ProCamCalibration/RealSenseServer/ConvertRGB24.cs
@@ -0,0 +1,101 @@
+﻿namespace RealSenseServer
+{
+    public class ConvertRGB24
+    {
+        public static byte[] To8BitGrayscale(byte[] inputImageBytes, int width, int height)
+        {
+            int inputStride = width * 3; // 3 bytes per pixel for 24-bit color (RGB)
+            int outputStride = width;    // 1 byte per pixel for 8-bit grayscale
+            byte[] outputImageBytes = new byte[width * height];
+
+            for (int y = 0; y < height; y++)
+            {
+                int inputOffset = y * inputStride;
+                int outputOffset = y * outputStride;
+
+                for (int x = 0; x < width; x++)
+                {
+                    // Convert RGB pixel to grayscale
+                    byte r = inputImageBytes[inputOffset + x * 3];
+                    byte g = inputImageBytes[inputOffset + x * 3 + 1];
+                    byte b = inputImageBytes[inputOffset + x * 3 + 2];
+                    byte grayValue = (byte)(0.299 * r + 0.587 * g + 0.114 * b);
+
+                    outputImageBytes[outputOffset + x] = grayValue;
+                }
+            }
+
+            return outputImageBytes;
+        }
+
+        public static byte[] ToYUY2(byte[] inputImageBytes, int width, int height)
+        {
+            int inputStride = width * 3; // 3 bytes per pixel for 24-bit color (RGB)
+            int outputStride = width * 2; // 2 bytes per pixel for YUY2
+            byte[] outputImageBytes = new byte[width * height * 2];
+
+            for (int y = 0; y < height; y++)
+            {
+                int inputOffset = y * inputStride;
+                int outputOffset = y * outputStride;
+
+                for (int x = 0; x < width; x += 2)
+                {
+                    // Convert two consecutive RGB pixels to YUV
+                    byte r1 = inputImageBytes[inputOffset + x * 3];
+                    byte g1 = inputImageBytes[inputOffset + x * 3 + 1];
+                    byte b1 = inputImageBytes[inputOffset + x * 3 + 2];
+
+                    byte r2 = inputImageBytes[inputOffset + (x + 1) * 3];
+                    byte g2 = inputImageBytes[inputOffset + (x + 1) * 3 + 1];
+                    byte b2 = inputImageBytes[inputOffset + (x + 1) * 3 + 2];
+
+                    byte y1 = (byte)(0.299 * r1 + 0.587 * g1 + 0.114 * b1);
+                    byte y2 = (byte)(0.299 * r2 + 0.587 * g2 + 0.114 * b2);
+
+                    byte u = (byte)(-0.14713 * r1 - 0.28886 * g1 + 0.436 * b1 + 128);
+                    byte v = (byte)(0.615 * r1 - 0.51499 * g1 - 0.10001 * b1 + 128);
+
+                    // Pack the Y, U, and Y, V values into YUY2 format
+                    outputImageBytes[outputOffset] = y1;
+                    outputImageBytes[outputOffset + 1] = u;
+                    outputImageBytes[outputOffset + 2] = y2;
+                    outputImageBytes[outputOffset + 3] = v;
+
+                    outputOffset += 4; // Move to the next YUY2 pixel
+                }
+            }
+
+            return outputImageBytes;
+        }
+
+        public static byte[] ToBGRA(byte[] inputImageBytes, int width, int height, byte alphaValue)
+        {
+            int inputStride = width * 3; // 3 bytes per pixel for 24-bit RGB
+            int outputStride = width * 4; // 4 bytes per pixel for BGRA
+            byte[] outputImageBytes = new byte[width * height * 4];
+
+            for (int y = 0; y < height; y++)
+            {
+                int inputOffset = y * inputStride;
+                int outputOffset = y * outputStride;
+
+                for (int x = 0; x < width; x++)
+                {
+                    // Extract RGB values from the input image
+                    byte r = inputImageBytes[inputOffset + x * 3];
+                    byte g = inputImageBytes[inputOffset + x * 3 + 1];
+                    byte b = inputImageBytes[inputOffset + x * 3 + 2];
+
+                    // Set BGRA values in the output image
+                    outputImageBytes[outputOffset + x * 4] = b; // Blue
+                    outputImageBytes[outputOffset + x * 4 + 1] = g; // Green
+                    outputImageBytes[outputOffset + x * 4 + 2] = r; // Red
+                    outputImageBytes[outputOffset + x * 4 + 3] = alphaValue; // Alpha
+                }
+            }
+
+            return outputImageBytes;
+        }
+    }
+}
--- ProCamCalibration/RealSenseServer/RealSenseServer.cs
+++ ProCamCalibration/RealSenseServer/RealSenseServer.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Threading;
 using System.ServiceModel;
 using System.ServiceModel.Discovery;
@@ -7,8 +6,9 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.IO;
 using Intel.RealSense;
-using System.Collections;
 using System.Threading.Tasks;
+using RealSenseServer;
+using SharpDX.WIC;
 
 namespace RoomAliveToolkit
 {
@@ -22,51 +22,149 @@ namespace RoomAliveToolkit
 
     public class RealSenseHandler
     {
-        const int Q_CAPACITY = 5; 
-
         public static RealSenseHandler instance;
-        public Pipeline pipe;
+        public Pipeline pipeline;
 
-        public FrameQueue depthQueue = new FrameQueue(Q_CAPACITY);
-        public ushort[] depthShortBuffer = new ushort[RealSenseCalibration.depthImageWidth * RealSenseCalibration.depthImageHeight];
+        // public FrameQueue depthQueue = new FrameQueue(Q_CAPACITY);
         public byte[] depthByteBuffer = new byte[RealSenseCalibration.depthImageWidth * RealSenseCalibration.depthImageHeight * 2];
-
-        /*
-        public FrameQueue colorQueue = new FrameQueue(Q_CAPACITY);
-        public ushort[] colorShortBuffer = new ushort[RealSenseCalibration.depthImageWidth * RealSenseCalibration.depthImageHeight];
-        public byte[] colorByteBuffer = new byte[RealSenseCalibration.depthImageWidth * RealSenseCalibration.depthImageHeight * 2];
-        */
-
         public List<AutoResetEvent> depthFrameReady = new List<AutoResetEvent>();
+
+        private byte[] rgbByteBuffer = new byte[RealSenseCalibration.colorImageWidth * RealSenseCalibration.colorImageHeight * 3];
+
+        public byte[] yuvByteBuffer = new byte[RealSenseCalibration.colorImageWidth * RealSenseCalibration.colorImageHeight * 2];
+        public List<AutoResetEvent> yuvFrameReady = new List<AutoResetEvent>();
+        public byte[] bgraByteBuffer = new byte[RealSenseCalibration.colorImageWidth * RealSenseCalibration.colorImageHeight * 4];
+        public List<AutoResetEvent> bgraFrameReady = new List<AutoResetEvent>();
+        public byte[] jpegByteBuffer = new byte[RealSenseCalibration.colorImageWidth * RealSenseCalibration.colorImageHeight * 4];
+        public List<AutoResetEvent> jpegFrameReady = new List<AutoResetEvent>();
+        public int nJpegBytes = 0;
+
+        ImagingFactory imagingFactory = new ImagingFactory();
+
+        void HandleDepthFrame(DepthFrame depth)
+        {
+            depth.CopyTo<byte>(depthByteBuffer);
+            lock (depthFrameReady)
+                foreach (var autoResetEvent in depthFrameReady)
+                    autoResetEvent.Set();
+        }
+
+        void HandleColorFrame(VideoFrame color)
+        {
+            color.CopyTo<byte>(rgbByteBuffer);
+
+            // BGRA
+            bgraByteBuffer = ConvertRGB24.ToBGRA(rgbByteBuffer, color.Width, color.Height, 255);
+            lock (bgraFrameReady)
+                foreach (var autoResetEvent in bgraFrameReady)
+                    autoResetEvent.Set();
+
+            // YUV
+            yuvByteBuffer = ConvertRGB24.ToYUY2(rgbByteBuffer, color.Width, color.Height);
+            lock (yuvFrameReady)
+                foreach (var autoResetEvent in yuvFrameReady)
+                    autoResetEvent.Set();
+
+            // JPEG
+            var bitmapSource = new Bitmap(imagingFactory, RealSenseCalibration.colorImageWidth, RealSenseCalibration.colorImageHeight, SharpDX.WIC.PixelFormat.Format32bppBGR, BitmapCreateCacheOption.CacheOnLoad);
+            var bitmapLock = bitmapSource.Lock(BitmapLockFlags.Write);
+            Marshal.Copy(bgraByteBuffer, 0, bitmapLock.Data.DataPointer, RealSenseCalibration.colorImageWidth * RealSenseCalibration.colorImageHeight * 4);
+            bitmapLock.Dispose();
+
+            var memoryStream = new MemoryStream();
+
+            var stream = new WICStream(imagingFactory, memoryStream);
+
+            var jpegBitmapEncoder = new JpegBitmapEncoder(imagingFactory);
+            jpegBitmapEncoder.Initialize(stream);
+
+            var bitmapFrameEncode = new BitmapFrameEncode(jpegBitmapEncoder);
+            bitmapFrameEncode.Options.ImageQuality = 0.5f;
+            bitmapFrameEncode.Initialize();
+            bitmapFrameEncode.SetSize(RealSenseCalibration.colorImageWidth, RealSenseCalibration.colorImageHeight);
+            var pixelFormatGuid = PixelFormat.FormatDontCare;
+            bitmapFrameEncode.SetPixelFormat(ref pixelFormatGuid);
+            bitmapFrameEncode.WriteSource(bitmapSource);
+
+            bitmapFrameEncode.Commit();
+            jpegBitmapEncoder.Commit();
+
+            lock (jpegByteBuffer)
+            {
+                nJpegBytes = (int)memoryStream.Length;
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                memoryStream.Read(jpegByteBuffer, 0, nJpegBytes);
+            }
+            lock (jpegFrameReady)
+                foreach (var autoResetEvent in jpegFrameReady)
+                    autoResetEvent.Set();
+
+            bitmapSource.Dispose();
+            memoryStream.Close();
+            memoryStream.Dispose();
+            stream.Dispose();
+            jpegBitmapEncoder.Dispose();
+            bitmapFrameEncode.Dispose();
+        }
 
         public RealSenseHandler()
         {
             instance = this;
-            pipe = new Pipeline();
-            pipe.Start();
+            pipeline = new Pipeline();
+            
+            using (var ctx = new Context())
+            {
+                var devices = ctx.QueryDevices();
+                var dev = devices[0];
+
+                Console.WriteLine("\nUsing device 0, an {0}", dev.Info[CameraInfo.Name]);
+                Console.WriteLine("    Serial number: {0}", dev.Info[CameraInfo.SerialNumber]);
+                Console.WriteLine("    Firmware version: {0}", dev.Info[CameraInfo.FirmwareVersion]);
+
+                var cfg = new Config();
+                cfg.EnableStream(Intel.RealSense.Stream.Depth, RealSenseCalibration.depthImageWidth, RealSenseCalibration.depthImageHeight, Format.Z16, 5);
+                cfg.EnableStream(Intel.RealSense.Stream.Color, RealSenseCalibration.colorImageWidth, RealSenseCalibration.colorImageHeight, Format.Rgb8, 5);
+
+                pipeline.Start(cfg);
+            }
+
+            var frameQueue = new FrameQueue(5); // allow max latency of 5 frames
 
             Task.Run(() =>
             {
                 while (true)
                 {
-                    using (var frames = pipe.WaitForFrames())
+                    DepthFrame frame;
+                    if (frameQueue.PollForFrame(out frame))
                     {
-                        using (var depth = frames.DepthFrame)
+                        using (frame)
                         {
-                            depth.CopyTo<ushort>(depthShortBuffer);
-                            lock (depthFrameReady)
-                                foreach (var autoResetEvent in depthFrameReady)
-                                    autoResetEvent.Set();
+                            if (frame.DataSize == depthByteBuffer.Length)
+                            {
+                                Console.WriteLine("depth frame");
+                                HandleDepthFrame(frame);
+                            }
+                            else
+                            {
+                                Console.WriteLine("color frame");
+                                HandleColorFrame(frame);
+                            }
                         }
-
-                        using (var color = frames.ColorFrame)
-                        {
-                            //color.
-
-                        }
-                                            }
+                    }
                 }
             });
+
+            while (true)
+            {
+                using (var frames = pipeline.WaitForFrames()) 
+                {
+                    using (var depth = frames.DepthFrame)
+                        frameQueue.Enqueue(depth);
+                    using (var color = frames.ColorFrame)
+                        frameQueue.Enqueue(color);
+                }
+                
+            }
         }
     }
 
@@ -82,11 +180,21 @@ namespace RoomAliveToolkit
         byte[] rgbByteBuffer = new byte[RealSenseCalibration.colorImageWidth * RealSenseCalibration.colorImageHeight * 4];
 
         AutoResetEvent depthFrameReady = new AutoResetEvent(false);
+        AutoResetEvent rgbFrameReady = new AutoResetEvent(false);
+        AutoResetEvent yuvFrameReady = new AutoResetEvent(false);
+        AutoResetEvent jpegFrameReady = new AutoResetEvent(false);
+
 
         public RealSenseServer()
         {
             lock (RealSenseHandler.instance.depthFrameReady)
                 RealSenseHandler.instance.depthFrameReady.Add(depthFrameReady);
+            lock (RealSenseHandler.instance.bgraFrameReady)
+                RealSenseHandler.instance.bgraFrameReady.Add(rgbFrameReady);
+            lock (RealSenseHandler.instance.yuvFrameReady)
+                RealSenseHandler.instance.yuvFrameReady.Add(yuvFrameReady);
+            lock (RealSenseHandler.instance.jpegFrameReady)
+                RealSenseHandler.instance.jpegFrameReady.Add(jpegFrameReady);
         }
 
         // Returns immediately if a frame has been made available since the last time this was called on this client;
@@ -95,29 +203,45 @@ namespace RoomAliveToolkit
         public byte[] LatestDepthImage()
         {
             depthFrameReady.WaitOne();
-            Console.WriteLine("depth frame");
+            Console.WriteLine("[LatestDepthImage]");
             // Is this copy really necessary?:
-            lock (RealSenseHandler.instance.depthShortBuffer)
-                Buffer.BlockCopy(RealSenseHandler.instance.depthShortBuffer, 0, depthByteBuffer, 0, RealSenseCalibration.depthImageWidth * RealSenseCalibration.depthImageHeight * 2);
+            lock (RealSenseHandler.instance.depthByteBuffer)
+                Buffer.BlockCopy(RealSenseHandler.instance.depthByteBuffer, 0, depthByteBuffer, 0, RealSenseCalibration.depthImageWidth * RealSenseCalibration.depthImageHeight * 2);
             return depthByteBuffer;
         }
 
         [OperationContract]
         public byte[] LatestYUVImage()
         {
-            return new byte[] { };
+            yuvFrameReady.WaitOne();
+            Console.WriteLine("[LatestYUVImage]");
+            lock (RealSenseHandler.instance.yuvByteBuffer)
+                Buffer.BlockCopy(RealSenseHandler.instance.yuvByteBuffer, 0, yuvByteBuffer, 0, RealSenseCalibration.colorImageWidth * RealSenseCalibration.colorImageHeight * 2);
+            return yuvByteBuffer;
         }
 
         [OperationContract]
         public byte[] LatestRGBImage()
         {
-            return new byte[] { };
+            rgbFrameReady.WaitOne();
+            Console.WriteLine("[LatestRGBImage]");
+            lock (RealSenseHandler.instance.bgraByteBuffer)
+                Buffer.BlockCopy(RealSenseHandler.instance.bgraByteBuffer, 0, rgbByteBuffer, 0, RealSenseCalibration.colorImageWidth * RealSenseCalibration.colorImageHeight * 4);
+            return rgbByteBuffer;
         }
 
         [OperationContract]
         public byte[] LatestJPEGImage()
         {
-            return new byte[] { };
+            jpegFrameReady.WaitOne();
+            Console.WriteLine("[LatestJPEGImage]");
+            byte[] jpegByteBuffer;
+            lock (RealSenseHandler.instance.jpegByteBuffer)
+            {
+                jpegByteBuffer = new byte[RealSenseHandler.instance.nJpegBytes];
+                Buffer.BlockCopy(RealSenseHandler.instance.jpegByteBuffer, 0, jpegByteBuffer, 0, RealSenseHandler.instance.nJpegBytes);
+            }
+            return jpegByteBuffer;
         }
     }
 
@@ -126,7 +250,7 @@ namespace RoomAliveToolkit
         static void Main(string[] args)
         {
             new RealSenseHandler();
-            var serviceHost = new ServiceHost(typeof(RealSenseServer));
+            var serviceHost = new ServiceHost(typeof(KinectServer2));
 
             // discovery
             serviceHost.Description.Behaviors.Add(new ServiceDiscoveryBehavior());
